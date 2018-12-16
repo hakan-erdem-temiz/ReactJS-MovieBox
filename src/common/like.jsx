65226ce4229cb8f8894f131127663d6c712ecade
hakan erdem <hkn.het@gmail.com>
2018-12-16 20:03:59 +0300

    turn it to stateless component
    it doesn’t have any state itself. We don’t have any state here. So i
    can simplify this.

--- src/common/like.jsx
+++ src/common/like.jsx
@@ -3,19 +3,32 @@ import React, { Component } from "react";
 // INput: liked: bolean
 // Output: onClick
 
-class Like extends Component {
-  render() {
-    let heartClassName = "fa fa-heart";
-    if (!this.props.likeData.liked) heartClassName = heartClassName + "-o";
-    return (
-      <i
-        className={heartClassName}
-        aria-hidden="true"
-        style={{ cursor: "pointer" }}
-        onClick={this.props.onLike}
-      />
-    );
-  }
-}
+const Like = props => {
+  let heartClassName = "fa fa-heart";
+  if (!props.likeData.liked) heartClassName = heartClassName + "-o";
+  return (
+    <i
+      className={heartClassName}
+      aria-hidden="true"
+      style={{ cursor: "pointer" }}
+      onClick={props.onLike}
+    />
+  );
+};
+
+// class Like extends Component {
+//   render() {
+//     let heartClassName = "fa fa-heart";
+//     if (!this.props.likeData.liked) heartClassName = heartClassName + "-o";
+//     return (
+//       <i
+//         className={heartClassName}
+//         aria-hidden="true"
+//         style={{ cursor: "pointer" }}
+//         onClick={this.props.onLike}
+//       />
+//     );
+//   }
+// }
 
 export default Like;
